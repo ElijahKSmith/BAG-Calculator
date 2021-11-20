853d7359dc23017d6a5bacda6844fa646e7a1c45
ElijahKSmith <squidtheprinter@gmail.com>
2021-11-19 22:18:58 -0600

    Prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarModule } from './components/navbar/navbar.module';
 import { StudentsModule } from './components/students/student.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule, NavbarModule, StudentsModule, BrowserAnimationsModule],
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    NavbarModule,
+    StudentsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
